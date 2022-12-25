08ef9d6cbe5ce9859a6d837b9f4a33d8a1dbb7b6
WiwilZ <997282029@qq.com>
2022-12-25 13:41:00 +0800

    add sound and fix some bugs

--- game.py
+++ game.py
@@ -1,6 +1,7 @@
 import math
 import random
 import sys
+from itertools import chain
 
 import pygame
 from pygame.locals import *
@@ -10,6 +11,18 @@ WIDTH, HEIGHT = 600, 800
 pygame.init()
 display_surface = pygame.display.set_mode((WIDTH, HEIGHT), pygame.DOUBLEBUF, vsync=1)
 pygame.display.set_caption("Doodle Jump")
+
+# 声音
+jump_sound = pygame.mixer.Sound('assets/sound/jump.wav')
+high_jump_sound = pygame.mixer.Sound('assets/sound/highJump2.wav')
+death_sound = pygame.mixer.Sound('assets/sound/death.wav')
+
+# 字体
+font = pygame.freetype.Font("assets/font/al-seana.ttf", 36)
+
+
+def load_image(filepath):
+    return pygame.image.load(f"assets/image/{filepath}.png").convert_alpha()
 
 
 class Entity:
@@ -36,11 +49,10 @@ class Background(Entity):
 
 
 class Player(Entity):
-    images = [pygame.image.load(f"assets/player/{name}.png").convert_alpha() for name in
-              ('idle_left', 'idle_right', 'jump_left', 'jump_right')]
+    images = [load_image(f"player/{name}") for name in ('idle_left', 'idle_right', 'jump_left', 'jump_right')]
     width, height = images[0].get_size()
     ACC = Vector2(0, 0.6)
-    INITIAL_SPEED_Y = 13
+    INITIAL_SPEED_Y = 15
 
     def __init__(self, pos: Vector2):
         super().__init__(self.images[0])
@@ -79,8 +91,7 @@ class Player(Entity):
 
 
 class Platform(Entity):
-    images = [pygame.image.load(f"assets/platform/{name}.png").convert_alpha() for name in
-              ('green', 'blue', 'red', 'broken')]
+    images = [load_image(f"platform/{name}") for name in ('green', 'blue', 'red', 'broken')]
     width, height = images[0].get_size()
 
     def __init__(self, pos: Vector2, type: int, level: int):
@@ -105,7 +116,7 @@ class Platform(Entity):
 
 
 class Spring(Entity):
-    images = [pygame.image.load(f"assets/spring/{name}.png").convert_alpha() for name in ('idle', 'released')]
+    images = [load_image(f"spring/{name}") for name in ('idle', 'released')]
     width, height = images[0].get_size()
 
     def __init__(self, pos: Vector2):
@@ -125,7 +136,8 @@ class Spring(Entity):
 
 class Game:
     FPS = 60
-    MAX_SPEED = 2.0
+    SPEED_UP = 0.05
+    MAX_SPEED = 4.0
 
     def __init__(self):
         self.clock = pygame.time.Clock()
@@ -138,14 +150,14 @@ class Game:
         self.springs = []
         self.platforms = []
 
-    def add_platform_and_spring(self, y):
+    def add_platform_and_spring(self, y: float):
         x = random.uniform(math.ceil(Platform.width / 2), math.floor(WIDTH - Platform.width / 2))
         # 绿、蓝、红按6 : 3 : 1的概率生成平台
         platform_type = random.choice([0] * 6 + [1] * 3 + [2])
         self.platforms.append(Platform(Vector2(x, y), platform_type, self.platform_level))
         # 绿色平台 1/5 概率生成弹簧
         if platform_type == 0 and random.randint(1, 5) == 1:
-            self.springs.append(Spring(Vector2(x + (Platform.width - Spring.width) / 2 * random.uniform(-1, 1), y + 2)))
+            self.springs.append(Spring(Vector2(x + (Platform.width - Spring.width) / 2 * random.uniform(-1, 1), y)))
         self.platform_level += 1
 
     def detect_collision(self):
@@ -162,6 +174,7 @@ class Game:
                 self.player.pos.y = platform.pos.y
                 self.player.level = max(platform.level, self.player.level)
                 self.player.stay = True
+                pygame.mixer.Sound.play(jump_sound)
 
         # 与弹簧碰撞
         for spring in self.springs:
@@ -169,11 +182,17 @@ class Game:
                     self.player.pos.x - spring.pos.x) < (Player.width + Spring.width) / 2 and not spring.is_released:
                 self.player.vel.y = -Player.INITIAL_SPEED_Y * 1.5
                 spring.set_released()
+                pygame.mixer.Sound.play(high_jump_sound)
 
     def run(self):
         self.reset()
         # 第一个平台位于角色出生位置
         self.platforms.append(Platform(Vector2(self.player.pos), 0, 0))
+        # 播放背景音乐
+        pygame.mixer.music.load("assets/sound/music.mp3")
+        pygame.mixer.music.set_volume(0.5)
+        pygame.mixer.music.play(-1)
+
         # 生成上面的平台
         for y in range(int(self.player.pos.y - 60), 0, -60):
             self.add_platform_and_spring(y)
@@ -184,39 +203,45 @@ class Game:
                     pygame.quit()
                     sys.exit()
 
-            for e in [self.background, self.player] + self.platforms + self.springs:
+            for e in chain((self.background, self.player), self.platforms, self.springs):
                 e.update()
                 e.draw(display_surface)
+
+            # 显示得分
+            font.render_to(display_surface, (450, 30), f"Score: {self.level}", fgcolor=(0, 0, 0), size=30)
 
             self.detect_collision()
 
             # 角色靠近上面立刻刷新平台
-            if self.player.pos.y - 120 <= 0 and self.player.stay:
+            if self.player.pos.y - 400 <= 0:
+                c = ((400 - self.player.pos.y) / 400)  # 距离屏幕上方越近就越靠近1
+                up_val = 20  # 额外下落的值
                 for e in self.platforms + self.springs:
-                    e.pos.y += HEIGHT - 120
-                self.player.pos.y += HEIGHT - 120
-                for y in range(int(self.player.pos.y - 120), 0, -60):
-                    self.add_platform_and_spring(y)
+                    e.pos.y += c * up_val
+                self.player.pos.y += c * up_val
 
             # 平台下落
-            val = min(self.level * 0.01, self.MAX_SPEED)
-            for e in self.platforms + self.springs:
+            val = min(self.level * self.SPEED_UP, self.MAX_SPEED)
+            for e in chain(self.platforms, self.springs):
                 e.pos.y += val
 
-            for i in range(len(self.platforms) - 1, -1, -1):
-                if self.platforms[i] and self.platforms[i].pos.y > HEIGHT:
-                    self.platforms.pop(i)
-            for i in range(len(self.springs) - 1, -1, -1):
-                if self.springs[i] and self.springs[i].pos.y > HEIGHT:
-                    self.springs.pop(i)
+            # 移除掉出屏幕的平台和弹簧
+            def rfind(iterable):
+                for i, e in enumerate(reversed(iterable)):
+                    if e.pos.y > HEIGHT:
+                        return iterable[-i:]
+                return iterable
+
+            self.platforms, self.springs = map(rfind, (self.platforms, self.springs))
 
             # 在上方新增平台
             y = self.platforms[-1].pos.y - 60
-            if y >= 0:
+            if y >= -60:
                 self.add_platform_and_spring(y)
 
             # 玩家掉出屏幕重新开始
             if self.player.rect.top > HEIGHT:
+                pygame.mixer.Sound.play(death_sound)
                 pygame.init()
                 self.run()
 
